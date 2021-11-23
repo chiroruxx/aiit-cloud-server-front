220f9894279d436e6e65e82a4456907c54b89d7f
chiroruxx <dormir.kazu@gmail.com>
2021-11-23 22:47:13 +0900

    書き起こしフォームを追加

--- src/main.vue
+++ src/main.vue
@@ -3,19 +3,33 @@
     <header class="header">
       <h1>Realtime Text</h1>
     </header>
-    <div class="container">
-      <template v-for="text in textList">
-        <div class="container__item">
-          <p class="text">
-            {{ text.origin }}
-          </p>
-        </div>
-        <div class="container__item">
-          <p class="text">
-            {{ text.translated }}
-          </p>
-        </div>
-      </template>
+    <div class="main">
+      <form @submit="startTranscribe" class="form">
+        <label>
+          ZoomID:
+          <input type="text" v-model="id">
+        </label>
+        <label>
+          Zoomパスワード:
+          <input type="text" v-model="password">
+        </label>
+        <input type="submit" value="書き起こし開始">
+        <input type="button" value="書き起こし終了" @click="stopTranscribe">
+      </form>
+      <div class="container">
+        <template v-for="text in textList">
+          <div class="container__item">
+            <p class="text">
+              {{ text.origin }}
+            </p>
+          </div>
+          <div class="container__item">
+            <p class="text">
+              {{ text.translated }}
+            </p>
+          </div>
+        </template>
+      </div>
     </div>
   </div>
 </template>
@@ -29,6 +43,8 @@ const endpoint = 'https://c0gm3sfkl4.execute-api.ap-northeast-1.amazonaws.com/ap
 export default {
   data: () => ({
     textList: [],
+    id: '',
+    password: '',
   }),
 
   mounted() {
@@ -52,8 +68,34 @@ export default {
                   translated: item.translated
                 }))
           })
-    }, 1 * 1000)
+    }, 1000)
   },
+  computed: {
+    startTranscribeUrl() {
+      return `http://ec2-35-76-174-90.ap-northeast-1.compute.amazonaws.com:9999/transtart?confno=${this.id}&confpwd=${this.password}`
+    },
+    stopTranscribeUrl() {
+      return 'http://ec2-35-76-174-90.ap-northeast-1.compute.amazonaws.com:9999/transtop'
+    }
+  },
+  methods: {
+    startTranscribe(e) {
+      console.log(this.startTranscribeUrl)
+      axios.get(this.startTranscribeUrl)
+          .then(() => {
+            console.log('start transcribe response is ok')
+          })
+
+      e.preventDefault()
+    },
+    stopTranscribe() {
+      console.log(this.stopTranscribeUrl)
+      axios.get(this.stopTranscribeUrl)
+          .then(() => {
+            console.log('stop transcribe response is ok')
+          })
+    },
+  }
 }
 </script>
 
@@ -64,12 +106,19 @@ export default {
   padding: 12px 4px;
 }
 
-.container {
-  display: grid;
-  grid-template-columns: 50% auto;
+.main {
   width: 80%;
   min-width: 640px;
   margin: 0 auto;
+}
+
+.form {
+  margin: 2rem 0;
+}
+
+.container {
+  display: grid;
+  grid-template-columns: 50% auto;
 
   &__item {
     padding-left: 4px;
